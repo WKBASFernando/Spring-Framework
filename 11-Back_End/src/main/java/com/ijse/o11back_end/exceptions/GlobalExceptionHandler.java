78b786e9f127ff9f63b6f7e3297e01cbac2f2aab
WKBASFernando <sankalpaangelo@gmail.com>
2025-07-24 11:57:44 +0530

    exception handelling

--- 11-Back_End/src/main/java/com/ijse/o11back_end/exceptions/GlobalExceptionHandler.java
+++ 11-Back_End/src/main/java/com/ijse/o11back_end/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.ijse.o11back_end.exceptions;
 import com.ijse.o11back_end.util.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,5 +28,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<APIResponse> handlesValidationException(ValidationException e){
         return new ResponseEntity<>(new APIResponse(400, e.getMessage(), null),
                 HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<APIResponse> handlesMethodArgumentNotValid(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            errors.put(error.getDefaultMessage(), error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(new APIResponse(400, e.getMessage(), errors), HttpStatus.BAD_REQUEST);
     }
 }
