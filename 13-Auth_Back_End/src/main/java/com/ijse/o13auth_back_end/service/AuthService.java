5a64562a067277f1e11c51270caf00a37162cfe2
WKBASFernando <sankalpaangelo@gmail.com>
2025-07-31 14:27:32 +0530

    auth service register

--- 13-Auth_Back_End/src/main/java/com/ijse/o13auth_back_end/service/AuthService.java
+++ 13-Auth_Back_End/src/main/java/com/ijse/o13auth_back_end/service/AuthService.java
@@ -2,6 +2,8 @@ package com.ijse.o13auth_back_end.service;
 
 import com.ijse.o13auth_back_end.dto.AuthDTO;
 import com.ijse.o13auth_back_end.dto.AuthResponseDTO;
+import com.ijse.o13auth_back_end.dto.RegisterDTO;
+import com.ijse.o13auth_back_end.entity.Role;
 import com.ijse.o13auth_back_end.entity.User;
 import com.ijse.o13auth_back_end.repository.UserRepository;
 import com.ijse.o13auth_back_end.util.JwtUtil;
@@ -26,5 +28,18 @@ public class AuthService {
         }
         String token = jwtUtil.generateToken(authDTO.getUsername());
         return new AuthResponseDTO(token);
+    }
+
+    public String register(RegisterDTO registerDTO) {
+        if (userRepository.findByUsername(registerDTO.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exists!");
+        }
+        User user = User.builder()
+                .username(registerDTO.getUsername())
+                .password(passwordEncoder.encode(registerDTO.getPassword()))
+                .role(Role.valueOf(registerDTO.getRole()))
+                .build();
+        userRepository.save(user);
+        return "User " + registerDTO.getUsername() + " registered successfully!";
     }
 }
