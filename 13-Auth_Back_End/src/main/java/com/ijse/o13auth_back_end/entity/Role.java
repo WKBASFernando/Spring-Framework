41997080389775b963dac347f6d4438c4a23b7d9
WKBASFernando <sankalpaangelo@gmail.com>
2025-07-31 11:27:32 +0530

    entity class

---
+++ 13-Auth_Back_End/src/main/java/com/ijse/o13auth_back_end/entity/Role.java
@@ -0,0 +1,5 @@
+package com.ijse.o13auth_back_end.entity;
+
+public enum Role {
+    ADMIN,USER
+}
---
+++ 13-Auth_Back_End/src/test/java/com/ijse/o13auth_back_end/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.o13auth_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
